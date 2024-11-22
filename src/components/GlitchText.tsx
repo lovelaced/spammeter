e3a958bf0236200e3cda99bb165b8003feb71f98
lovelaced <erin@parity.io>
2024-11-22 10:40:42 +0000

    ramp down glitchtext

--- src/components/GlitchText.tsx
+++ src/components/GlitchText.tsx
@@ -10,7 +10,7 @@ export const GlitchText: React.FC<GlitchTextProps> = ({ text, tps }) => {
 
   useEffect(() => {
     const glitchChars = '!<>-_\\/[]{}—=+*^?#________';
-    const intensity = Math.min(tps / 50000, 1); // Normalize TPS to 0-1 range
+    const intensity = Math.min(tps / 5000000, 1); // Normalize TPS to 0-1 range
     const randomFactor = Math.random() * 100; // Random value between 0 and 100
     const speed = Math.max(50, 500 - tps / 50 + randomFactor); // Adjust speed based on TPS with a random element
     const interval = setInterval(() => {
